6977994ef036edce60e238394c29e0ea3f4644ed
GurungRegina <gina524286@gmail.com>
2023-10-04 17:42:02 +0100

    Create execute_python_pipeline.py

---
+++ dags/execute_python_pipeline.py
@@ -0,0 +1,47 @@
+import sys
+import os
+
+sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'src'))
+
+from datetime import datetime, timedelta
+from airflow.utils.dates import days_ago
+
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+
+from get_insurance_data import read_csv, remove_null_values, groupby_smoker, groupby_region
+
+default_args = {
+    'owner': 'rgurung'
+}
+
+with DAG(
+    dag_id = 'python_pipeline',
+    description = 'Running a Python pipeline',
+    default_args = default_args,
+    start_date = days_ago(1),
+    schedule_interval = '@once',
+    tags = ['pipeline', 'python', 'pandas']
+) as dag:
+    
+    read_csv_file = PythonOperator(
+        task_id = 'read_csv_file',
+        python_callable = read_csv
+    )
+
+    remove_null_values = PythonOperator(
+        task_id = 'remove_null_values',
+        python_callable = remove_null_values
+    )
+
+    groupby_smoker = PythonOperator(
+        task_id = 'groupby_smoker',
+        python_callable = groupby_smoker
+    )
+
+    groupby_region = PythonOperator(
+        task_id = 'groupby_region',
+        python_callable = groupby_region
+    )
+
+read_csv_file >> remove_null_values >> [groupby_smoker, groupby_region]
